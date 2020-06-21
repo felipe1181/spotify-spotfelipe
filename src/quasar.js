39a9be5a9fa7c09a4a80cb0d44891accdbf513f8
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-21 09:04:16 -0300

    commit initial

---
+++ src/quasar.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+
+import './styles/quasar.sass';
+import iconSet from 'quasar/icon-set/fontawesome-v5';
+import lang from 'quasar/lang/pt-br';
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar';
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  lang,
+  iconSet,
+});
