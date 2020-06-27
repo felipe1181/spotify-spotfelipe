1d578662b8f87040d39d9ae8aa2e35237a4e2926
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-27 13:01:44 -0300

    feat(footer/header): create component generics header/footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-footer elevated class="bg-SpotifyTertiary footer-container text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        <div class="row">
+          <div class="col-4 q-pa-md">
+            <ActiveAlbum title="Título música" artist="artista" />
+          </div>
+          <div class="col-4">
+            <div class="absolute-center">
+              Player
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="float-right">
+              Volume
+            </div>
+          </div>
+        </div>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+import ActiveAlbum from '@/views/player/pages/active-album/ActiveAlbum.vue';
+
+export default {
+  name: 'Footer',
+  components: {
+    ActiveAlbum,
+  },
+};
+</script>
+
+<style scoped>
+.footer-container {
+  height: 100px;
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-toolbar class="bg-SpotifyPrimaryAlternative">
+    <q-toolbar-title />
+    <q-btn class="vertical-middle bg-SpotifySecondary" no-caps rounded flat dense>
+      <div class="btn-perfil">
+        <q-avatar>
+          <img src="https://i.imgur.com/TQlsJtD.jpg" />
+        </q-avatar>
+        <span class="text">
+          Felipe ferraresi
+        </span>
+        <q-icon name="fas fa-caret-down" />
+      </div>
+    </q-btn>
+  </q-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.q-toolbar {
+  padding: 10px;
+}
+.btn-perfil {
+  flex: 1;
+  padding: 2px;
+  flex-direction: row;
+  justify-content: center;
+  background-color: #010101;
+  border-radius: 50px;
+}
+.btn-perfil:hover {
+  background-color: #2a2a2a;
+  transition: 0.4s;
+}
+.btn-perfil .q-icon {
+  margin: 0;
+}
+.btn-perfil .text {
+  font-weight: bold;
+}
+</style>
