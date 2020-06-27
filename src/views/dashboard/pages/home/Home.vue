e5fd68a5aee24b9407aca63b525dfdb4657a2628
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-27 13:02:36 -0300

    refactor(home): move home into dashboard paste

--- src/views/dashboard/pages/home/Home.vue
+++ src/views/dashboard/pages/home/Home.vue
@@ -1,11 +1,32 @@
 <template>
-  <h1>Home</h1>
+  <div class="container">
+    <div class="scroll-container">
+      <div class="container-list-card scroll items-center q-gutter-xl">
+        <ListCardMusic title="Tocado recentemente" />
+        <ListCardMusic title="Não sai dos seus ouvidos" />
+        <ListCardMusic title="Favoritas" />
+        <q-space v-for="index in 6" :key="index" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ListCardMusic from './components/list-card-music/ListCardMusic.vue';
+
 export default {
   name: 'Home',
+  components: {
+    ListCardMusic,
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.container-list-card {
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  height: 120vh;
+}
+</style>
