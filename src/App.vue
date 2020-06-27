1dded148fad069be076bf9b7d1a276024a7f93a1
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-27 13:00:44 -0300

    feat(app): organize and create components footer, header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <q-layout view="lHh lpR fFf">
+  <q-layout class="bg-SpotifySecondary" view="lHh lpR fFf">
     <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left" />
-
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-      </q-toolbar>
-
+      <Header />
       <q-tabs align="left">
         <q-route-tab to="/page1" label="Page One" />
         <q-route-tab to="/page2" label="Page Two" />
@@ -19,30 +9,21 @@
       </q-tabs>
     </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer show-if-above v-model="left" :width="230" side="left">
       <Sidebar />
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="container-main-dashboard">
       <router-view />
     </q-page-container>
-
-    <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
-          </q-avatar>
-          teste
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
+    <Footer />
   </q-layout>
 </template>
 
 <script>
-import Sidebar from './views/dashboard/pages/home/components/sidebar/Sidebar.vue';
+import Header from '@/components/Header/Header.vue';
+import Footer from '@/components/Footer/Footer.vue';
+import Sidebar from '@/views/dashboard/pages/home/components/sidebar/Sidebar.vue';
 
 export default {
   data() {
@@ -52,6 +33,62 @@ export default {
   },
   components: {
     Sidebar,
+    Header,
+    Footer,
   },
 };
 </script>
+
+<style lang="scss">
+@import '~@/styles/quasar.sass';
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+body {
+  overflow-y: hidden;
+}
+.container-main-dashboard {
+  overflow: hidden;
+}
+.bg-SpotifyPrimarySystem {
+  background-color: $spotufyPrimarySytem;
+}
+.color-SpotifyPrimarySystem {
+  color: $spotufyPrimarySytem;
+}
+.bg-SpotifyPrimary {
+  background-color: $spotifyPrimary;
+}
+.color-SpotifyPrimary {
+  color: $spotifyPrimary;
+}
+.bg-SpotifyPrimaryAlternative {
+  background-color: $spotifyPrimaryAlternative;
+}
+.color-SpotifyPrimaryAlternative {
+  color: $spotifyPrimaryAlternative;
+}
+.bg-SpotifySecondary {
+  background-color: $spotifySecondary;
+}
+.color-SpotifySecondary {
+  color: $spotifySecondary;
+}
+.bg-SpotifyTertiary {
+  background-color: $spotifyTertiary !important;
+}
+.color-SpotifyTertiary {
+  color: $spotifyTertiary;
+}
+.bg-SpotifyLight {
+  background-color: $spotifyLight;
+}
+.color-SpotifyLight {
+  color: $spotifyLight;
+}
+.bg-SpotifyDarkLight {
+  background-color: $spotifyDarkLight;
+}
+.color-SpotifyDarkLight {
+  color: $spotifyDarkLight;
+}
+</style>
