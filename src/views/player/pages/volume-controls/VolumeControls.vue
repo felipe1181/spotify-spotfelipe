b907053f46f0ec095770c260120b882a30a60cd4
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-27 21:15:50 -0300

    feat(volumeControls): new component volumeControls

---
+++ src/views/player/pages/volume-controls/VolumeControls.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div>
+      <q-btn flat dense round icon="fas fa-random"></q-btn>
+    </div>
+    <div>
+      <q-linear-progress dark :value="0.3" color="black" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VolumeControls',
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  background-color: blue;
+}
+.container .content {
+}
+</style>
