a22251b649e7fa6400b13351fc6603c8f12606a5
felipe ferraresi <felipe.ferraresi@rscinovacao.com.br>
2020-06-27 21:15:20 -0300

    feat(player): add new component, player

---
+++ src/views/player/pages/player/Player.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row q-gutter-md">
+    <div class="col-12 q-gutter-md">
+      <q-btn flat dense round icon="fas fa-random"></q-btn>
+      <q-btn flat dense round icon="fas fa-step-backward"></q-btn>
+      <q-btn outline round icon="fas fa-play"></q-btn>
+      <q-btn flat dense round icon="fas fa-step-forward"></q-btn>
+      <q-btn flat dense round icon="fas fa-redo-alt"></q-btn>
+    </div>
+    <div class="col-12">
+      <q-linear-progress dark :value="0.3" color="black" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+};
+</script>
+
+<style></style>
